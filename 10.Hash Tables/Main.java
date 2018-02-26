27f69904a2025eeaee42791663018a81c69483fc
Atanas <naskoraist@gmail.com>
2018-02-26 16:07:01 +0200

    HashTable exercises

--- 10.Hash Tables/Main.java
+++ 10.Hash Tables/Main.java
@@ -2,14 +2,6 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        TreeMap<String, Integer> map = new TreeMap<>();
-        map.put("nasko", 5);
-        map.put("nasko0", 1);
-        map.put("naskoo9o", 3);
-        map.put("simo", 55);
-        map.put("ivo", -55);
-        map.put("slavi", -32213);
-        map.put("pavkel", 32);
-        map.put("bobi", 111);
+
     }
 }
